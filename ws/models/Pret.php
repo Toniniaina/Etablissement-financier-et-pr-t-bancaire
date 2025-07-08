2f9b31d0c46d0b3ba84c6d7866469b21f84eca62
Odilah <odilahrandrianjanahary@hotmail.com>
2025-07-08 09:04:23 +0300

    commentiare

--- ws/models/Pret.php
+++ ws/models/Pret.php
@@ -4,22 +4,17 @@ require_once __DIR__ . '/../db.php';
 require_once __DIR__ . '/StatusPret.php';
 
 class Pret {
-    // Récupérer tous les prêts
     public static function getAll() {
         $db = getDB();
         $stmt = $db->query("SELECT * FROM Prets");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
-    // Récupérer un prêt par son ID
     public static function getById($id_prets) {
         $db = getDB();
         $stmt = $db->prepare("SELECT * FROM Prets WHERE id_prets = ?");
         $stmt->execute([$id_prets]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
-
-    // Créer un nouveau prêt
     public static function create($data) {
         $db = getDB();
         $stmt = $db->prepare("INSERT INTO Prets 
